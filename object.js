49a4c41bd833ddb2aca59c3eab2a363a0290f00f
harshgurjar977 <harshgurjar977@gmail.com>
2023-08-10 22:44:30 +0530

    nodejs

---
+++ object.js
@@ -0,0 +1,6 @@
+const user = {
+    name:'harsh',
+    city: 'ratlam',
+    age:23
+}
+console.log(user.name)
---
+++ student.js
@@ -0,0 +1,19 @@
+// console.log('harsh'); 
+// const age = 20;
+// const college = "itm";
+// module.exports.a = age;
+// module.exports.c = college;
+
+module.exports = {
+    name: 'ram',
+    age:23,
+    city:'Ratlam',
+    college: 'Medi',
+    addition:function(a,b){
+        console.log("its a function",a+b);
+    },
+    substraction:function(){
+        console.log("its a substraction");
+    }
+
+}
